3b9bfa326376224dea9bbd4dbbc65ddbff2b16d2
Justin <ja624@njit.edu>
2021-02-16 19:05:08 -0500

    First commit

---
+++ js/products.js
@@ -0,0 +1,73 @@
+(function() {
+    var mockDatabase = [
+        {
+            title: 'Razer Black Widow',
+            price: '$100',
+            id: 0,
+            tag:'razer',
+            img: 'assets/'
+        },
+        {
+            title: 'Razer Huntsman',
+            price: '$120',
+            id: 1,
+            tag:'razer',
+            img: 'assets/'
+        },
+        {
+            title: 'Razer Huntsman Mini',
+            price: '$90',
+            id: 2,
+            tag: 'razer',
+            img: 'assets/'
+        },
+        {
+            title: 'HyperX Alloy Origins',
+            price: '$92',
+            id: 3,
+            tag: "hyperx",
+            img: 'assets/'
+        },
+        {
+            title: 'HyperX Alloy FPS RGB',
+            price: '$110',
+            id: 4,
+            tag: "hyperx",
+            img: 'assets/'
+        },
+        {
+            title: 'HyperX Alloy FPS Pro',
+            price: '$70',
+            id: 5,
+            tag:'hyperx',
+            img: 'assets/'
+        },
+        {
+            title: 'Corsair K70 RGB',
+            price: '$150',
+            id: 6,
+            tag:'corsair',
+            img: 'assets/'
+        },
+        {
+            title: 'Corsair K95 RGB',
+            price: '$200',
+            id: 7,
+            tag:'corsair',
+            img: 'assets/'
+        },
+        {
+            title: 'Corsair Strafe',
+            price: '$95',
+            id: 8,
+            tag:'corsair',
+            img: 'assets/products-photos/yellow_vneck.jpg'
+        },
+        {
+            title: `Priceless Antique Keyboard`,
+            price: '$1000',
+            id: 9,
+            tag:'antique',
+            img: 'assets/'
+        },
+    ]
