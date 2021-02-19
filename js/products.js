e7db9006647f5a25dc88f649ab560e525c9c9367
Justin <ja624@njit.edu>
2021-02-18 19:57:13 -0500

    Adding media, first attempt at displaying products

--- js/products.js
+++ js/products.js
@@ -1,73 +1,121 @@
 (function() {
-    var mockDatabase = [
+    let mockDatabase = [
         {
             title: 'Razer Black Widow',
             price: '$100',
             id: 0,
-            tag:'razer',
-            img: 'assets/'
+            tag: 'razer',
+            img: 'media/rbw.jpg'
         },
         {
-            title: 'Razer Huntsman',
+            title: 'Razer Huntsman TE',
             price: '$120',
             id: 1,
-            tag:'razer',
-            img: 'assets/'
+            tag: 'razer',
+            img: 'media/rhte.jpg'
         },
         {
             title: 'Razer Huntsman Mini',
             price: '$90',
             id: 2,
             tag: 'razer',
-            img: 'assets/'
+            img: 'media/rhm.jpg'
         },
         {
             title: 'HyperX Alloy Origins',
             price: '$92',
             id: 3,
             tag: "hyperx",
-            img: 'assets/'
+            img: 'media/hxo.jpg'
         },
         {
             title: 'HyperX Alloy FPS RGB',
             price: '$110',
             id: 4,
             tag: "hyperx",
-            img: 'assets/'
+            img: 'media/hxfr.jpg'
         },
         {
             title: 'HyperX Alloy FPS Pro',
             price: '$70',
             id: 5,
-            tag:'hyperx',
-            img: 'assets/'
+            tag: 'hyperx',
+            img: 'media/hxfp.jpg'
         },
         {
             title: 'Corsair K70 RGB',
             price: '$150',
             id: 6,
-            tag:'corsair',
-            img: 'assets/'
+            tag: 'corsair',
+            img: 'media/ck70.jpg'
         },
         {
             title: 'Corsair K95 RGB',
             price: '$200',
             id: 7,
-            tag:'corsair',
-            img: 'assets/'
+            tag: 'corsair',
+            img: 'media/ck95'
         },
         {
             title: 'Corsair Strafe',
             price: '$95',
             id: 8,
-            tag:'corsair',
-            img: 'assets/products-photos/yellow_vneck.jpg'
+            tag: 'corsair',
+            img: 'media/products-photos/cs.jpg'
         },
         {
             title: `Priceless Antique Keyboard`,
             price: '$1000',
             id: 9,
-            tag:'antique',
-            img: 'assets/'
+            tag: 'antique',
+            img: 'media/poskb.jpg'
         },
-    ]
+    ];
+
+    function renderKey(results) {
+        const prodDiv = document.querySelector('#prodList');
+        prodCon.innerHTML = '';
+
+        const products = results.map((product) =>
+            `<div class="card">
+                <img src="${product.img}">
+                <h1>${product.title}</h1>
+                <h2>${product.price}</h2>
+             </div>
+            `
+        )
+
+        products.forEach((product) => {
+            prodCon.innerHTML += product;
+        });
+
+        renderKey(mockDatabase);
+    }
+
+    /*function renderKey(results) {
+        const prodDiv = document.querySelector('#prodList');
+        prodCon.innerHTML = '';
+
+        const products = results.map(function (product, index) {
+            return (
+                `
+                <div class="product">
+                    <img class="prodIMG" src="${product.img}" id="${index}" alt="kbpic">
+                    <div class="prodInfo">
+                        <h1 class="prodTitle">${product.title}</h1>
+                        <h2 class="prodPrice">${product.price}</h2>
+                    </div>
+                </div>
+            `
+            )
+        });
+
+        products.forEach(function (product) {
+            prodDiv.innerHTML += product
+        });
+
+        renderKey(mockDatabase);
+    }*/
+
+})();
+
